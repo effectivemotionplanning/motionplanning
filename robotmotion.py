3b755f8390d7fc5a2533dd011162955146dce58e
ArkhamKnight9103 <nihal.bhatnagar@gmail.com>
2020-07-23 02:34:17 -0500

    Added random map generation

--- robotmotion.py
+++ robotmotion.py
@@ -1,6 +1,30 @@
-from random import seed
+import keras
+from keras.layers import Input, Conv2D, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model, load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 import random
-from numpy import *
-##initializing matrix
+import numpy as np
+import pandas
 
+import matplotlib.pyplot as plt
+
+##initializing matrix
+m =  []
+for x in range(500):
+    m.append(np.zeros(500))
+
+for x in range(500):
+    row =  np.zeros(500)
+    for y in range(500):
+        num = random.random()
+        if num<=.01:
+            for z in range(5):
+                for a in range(5):
+                    holder = m[x-z]
+                    holder[y-a] = 1
+
+        else:
+            row[y] = 0
+    m[x] = row
 print(m)
