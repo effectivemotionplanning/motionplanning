4cce193584ab8a918633d99a473e7bd32992df99
pronay22 <pranay2203@gmail.com>
2020-07-28 13:14:14 -0500

    asadsAASD

--- robotmotion.py
+++ robotmotion.py
@@ -14,21 +14,67 @@ def generateArm1CurrentPosition(angle): ##defines the function for rotation
     mylist = np.zeros((500,500))
     for z in range(20):
         for c in range(arm1length):
-            mylist[499 - (240+c)][z+250] =1
-
+            mylist[499 - (240+c)][z+250] = 1
     angle = math.radians(angle) ##converts into radians
     matrix = np.zeros((500,500))  ##fill a 500x500 array with 0s
     for x in range(500): ##x coordinate
         for y in range(500): ##y coordinate
-            if mylist[499-y][x] == 1:##this is the arm and this code does rotation and at every point that equals one, it rotates it and creates a new matrix
-                center_x = 249
+            if mylist[499-y][x] == 1:##this is the arm and this code does rotation and at every point that equals one (which is the arm 1), it rotates it and creates a new matrix
+                center_x = 249 ##rotation around the center
                 center_y = 249
                 xprime = (x - center_x) * math.cos(angle) - (y - center_y) * math.sin(angle) + center_x
                 yprime = (x - center_x) * math.sin(angle) + (y - center_y) * math.cos(angle) + center_y
                 xprime = round(xprime)
                 yprime = round(yprime)
-                matrix[499 - yprime][xprime] = 1
+                matrix[499 - yprime][xprime] = 1 ##marks the rotated arm
     return matrix  ##returns the matrix with the rotated arm
+
+def doesarm1overlap(angle, coordinateList): ##defines the function for rotation
+    ##mylist = np.zeros((500,500))
+    array = []
+    for z in range(20):
+        for c in range(arm1length):
+            ##mylist[499 - (240+c)][z+250] = 1
+            holderArray = [] ## code should be adding coordinates of arm 2 (prior rotation)into the 2d array named array
+            holderArray.append(499-(c+yprime))
+            holderArray.append(startingx+z)
+            array.append(holderArray)
+    angle = math.radians(angle) ##converts into radians
+    ##matrix = np.zeros((500,500))  ##fill a 500x500 array with 0s
+    ##for x in range(500): ##x coordinate
+        ##for y in range(500): ##y coordinate
+            ##if mylist[499-y][x] == 1:##this is the arm and this code does rotation and at every point that equals one (which is the arm 1), it rotates it and creates a new matrix
+    for f in array:
+                x = f[0]
+                y = f[1]
+                center_x = 249 ##rotation around the center
+                center_y = 249
+                xprime = (x - center_x) * math.cos(angle) - (y - center_y) * math.sin(angle) + center_x
+                yprime = (x - center_x) * math.sin(angle) + (y - center_y) * math.cos(angle) + center_y
+                xprime = round(xprime)
+                yprime = round(yprime)
+                ##matrix[499 - yprime][xprime] = 1 ##marks the rotated arm
+                ##for z in range(20):
+                    ##for c in range(arm1length):
+                    ##    holderArray = [] ## code should be adding coordinates of arm 1 (after rotation) into the 2d array named array
+                        ##holderArray.append(499-(c+yprime))
+                        ##holderArray.append(xprime+z)
+                        ##array.append(holderArray)
+                for p in coordinateList:
+                    a = p[0]
+                    b = p[1]
+                    if array2[a][b] == 1: ##2d array
+                        return True
+                return False
+
+#prints total overlapped pixels of arm and matrix
+def overlapcount(armgrid, matrix):
+    overlapCount = 0
+    for x in range (500):
+        for y in range(500):
+            if armgrid[x,y] == 1 and matrix[x,y] ==1: ##if x and y corodiantes point to a place that is 1, then it will increase the overlap count to determine the number of collisions
+                overlapCount+=1
+    print(overlapCount)
 
 #prints how many obstacles are there
 def countobstacles(matrix):
@@ -42,9 +88,8 @@ def countobstacles(matrix):
 #generates array with arm2 position based on Arm1 configuration
 def generateArm2CurrentPosition(currentArm1Angle,currentArm2Angle):
     arm2holderGrid = np.zeros((500,500))
-
     angle = math.radians(currentArm1Angle)
-    center_x = 249
+    center_x = 249 ##rotation around the center to see where the first arm is
     center_y = 249
     x = 249
     y = 349
@@ -55,12 +100,12 @@ def generateArm2CurrentPosition(currentArm1Angle,currentArm2Angle):
     startingx = xprime - 10
     for z in range(20):
         for c in range(arm2length):
-            arm2holderGrid[499 - (c+yprime)][startingx+z] =1
+            arm2holderGrid[499 - (c+yprime)][startingx+z] = 1 ##marks the unrotated second arm
     newRotatedArm2Grid = np.zeros((500,500))
     angle =math.radians(currentArm2Angle)
     for x in range(500): ##x coordinate
         for y in range(500): ##y coordinate
-            if arm2holderGrid[499-y][x] == 1:##this is the arm and this code does rotation and at every point that equals one, it rotates it and creates a new matrix
+            if arm2holderGrid[499-y][x] == 1:##begins the rotation for the second arm around the point for the first arm
                 ox = xprime
                 oy = yprime
                 px = x
@@ -69,19 +114,15 @@ def generateArm2CurrentPosition(currentArm1Angle,currentArm2Angle):
                 qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
                 qx = round(qx)
                 qy = round(qy)
-                newRotatedArm2Grid[499-qy][qx] = 1
-
-
+                newRotatedArm2Grid[499-qy][qx] = 1 ##marks the rotated second arm
     return newRotatedArm2Grid
-#so bassically, we were able to reduce from like 9 hrs to 10 min per cspace by doing following changes:
-#first we changed it so that the cspace generation method  doesn't have to call arm 2 generation and isoverlap, because each iterated thru  500x500
-# this made it so that we only had to go thru 500 by 500 once.
-    #originally, we generated arm 2's current position by iterating thru a 500 by 500 matrix. then , to check overlap, we iterated thru 500 by 500 matrix again to see if there were collisisons between arm matrix and obstacle matrix
-    #we changed this so that while generating the 500 by 500 arm matrix itself, we check thru the obstacle list, that way we do not have to iterate thru a 500 by 500 matrix again
-    #this wasnt neccessary for arm 1, sicne although we iterated 500 by 500 in arm 1 generation, when checking for overlap, we iterated thru obstacle coordinate list, and used the coordinates from there to access values in arm 1's 500 by 500 grid and seeing if those values also equalled 1, so we actually were only iterating thru 500 by 500 once, and thus there was not much to decrease
-#then we also made it so that we only had to iterate thru arm coordinates (20 by 100) and the obstacle coordinates specifically, not the whole 500 by 500 matrixes
 
-def doesarm2overlap(currentArm1Angle,currentArm2Angle, obstaclelist):
+
+#so bassically, we were able to reduce cspace generation from like 9 hrs to 10 min per cspace by doing following changes:
+#first we changed it so that the cspace generation method  doesn't have to call arm 2 generation and isoverlap, because each iterated thru 500 by 500x500
+# this made it so that we only had to go thru 500 by 500 once.
+#then we also made it so that we only had to iterate thru arm coordinates (20 by 100) and the obstacle coordinates specifically, not the whole 500 by 500 matrixes
+def doesarm2overlap(currentArm1Angle,currentArm2Angle,obstaclelist): ##this method is basically the generatearm2 with a built in checker to determine collisions with obstacles during generation
     #arm2holderGrid = np.zeros((500,500))
     array = []
     #finalarray = []
@@ -98,12 +139,10 @@ def doesarm2overlap(currentArm1Angle,currentArm2Angle, obstaclelist):
     for z in range(20):
         for c in range(arm2length):
             #arm2holderGrid[499 - (c+yprime)][startingx+z] =1
-            holderArray = []
-            holderArray.append(c+yprime)
+            holderArray = [] ## code should be adding coordinates of arm 2 (prior rotation)into the 2d array named array
+            holderArray.append(499-(c+yprime))
             holderArray.append(startingx+z)
             array.append(holderArray)
-
-            ## code above should be adding coordinates of arm 2 (prior rotation)into the 2d array named array
     #newRotatedArm2Grid = np.zeros((500,500))
     angle =math.radians(currentArm2Angle)
     for f in array:
@@ -117,16 +156,14 @@ def doesarm2overlap(currentArm1Angle,currentArm2Angle, obstaclelist):
         qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
         qx = round(qx)
         qy = round(qy)
-        for r in obstaclelist:
-            if r[0] ==qy and r[1] ==qx:
-                return False
-        return True
+        for r in obstaclelist: ##checks the obstacles during generation to speed it up
+            if r[0] == qy and r[1] == qx: ##checks if row 1 and 2 in the obstaclelist are the same as the rotated arm
+                return True
+        return False
         #xxholderarray = []
         #xxholderarray.append(qx)
         #xxholderarray.append(qy)
         #finalarray.append(xxholderarray)
-
-
 
 count = 0
 def generateObstacleGrid():
@@ -138,29 +175,25 @@ def generateObstacleGrid():
             if num<.00003:
                 for r in range(20):
                     for c in range(20):
-                        m[499 - (y+r)][x+c] = 1
+                        m[y+r][x+c] = 1
                         holderArray = []
                         holderArray.append(y+r)
                         holderArray.append(x+c)
-                        array.append(holderArray)
+                        array.append(holderArray) ##saving the coordinates of the obstacles
     return m, array
 
 
-
-
-def generatecspace(obstacleArray, obstacleCoordinateList):
+def generatecspace(obstacleArray, coordinateList, obstacleCoordinateList):
     cspaceHolderGrid = np.zeros((360,360))
     for arm1degree in range(360):
-        arm1array = generateArm1CurrentPosition(arm1degree)
-        if isOverlap1(obstacleArray, obstacleCoordinateList, arm1array):
+        if doesarm1overlap(arm1degree, coordinateList):
             continue
         for arm2degree in range(360):
             print(arm2degree)
             if doesarm2overlap(arm1degree, arm2degree, obstaclelist):
-                cspaceHolderGrid[359 - arm2degree][arm1degree] = 1
+                cspaceHolderGrid[360 - arm2degree][arm1degree] = 1
             else:
-                cspaceHolderGrid[359 - arm2degree][arm1degree] = 0
-    print(cspaceHolderGrid)
+                cspaceHolderGrid[360 - arm2degree][arm1degree] = 0
     return cspaceHolderGrid
 
 
@@ -173,7 +206,7 @@ def isOverlap(array1, coordinateList, array2, array3):
     for p in coordinateList:
         a = p[0]
         b = p[1]
-        if array2[a][b] == 1 or array3[a][b] == 1:
+        if array2[a][b] == 1 or array3[a][b] == 1: ##2d array
             return True
     return False
 
@@ -186,7 +219,7 @@ def isOverlap1(array1, coordinateList, array2):
     for p in coordinateList:
         a = p[0]
         b = p[1]
-        if array2[499-a][b] == 1:
+        if array2[a][b] == 1: ##2d array
             return True
     return False
 
